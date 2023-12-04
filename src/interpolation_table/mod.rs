cfc4fda55abf191dd147b3b42ec736633319aad2
Sebastian Schmidt <isibboi@gmail.com>
2023-12-04 15:22:46 +0200

    Add more data about water.

--- src/interpolation_table/mod.rs
+++ src/interpolation_table/mod.rs
@@ -1,19 +1,41 @@
+pub enum LimitBehaviour {
+    Clamp,
+    Panic,
+}
+
 pub struct LinearInterpolationTable {
     table: Vec<(f64, f64)>,
+    limit_behaviour: LimitBehaviour,
 }
 
 impl LinearInterpolationTable {
-    pub fn new(table: Vec<(f64, f64)>) -> Self {
+    pub fn new(limit_behaviour: LimitBehaviour, table: Vec<(f64, f64)>) -> Self {
         assert!(!table.is_empty());
         assert!(table.windows(2).all(|pair| pair[0].0 < pair[1].0));
 
-        Self { table }
+        Self {
+            table,
+            limit_behaviour,
+        }
     }
 
     pub fn get(&self, x: f64) -> f64 {
         assert!(x.is_normal() || x == 0.0 || x == -0.0);
-        assert!(x >= self.table.first().unwrap().0);
-        assert!(x <= self.table.last().unwrap().0);
+
+        match self.limit_behaviour {
+            LimitBehaviour::Clamp => {
+                if x < self.table.first().unwrap().0 {
+                    return self.table.first().unwrap().1;
+                }
+                if x > self.table.last().unwrap().0 {
+                    return self.table.last().unwrap().1;
+                }
+            }
+            LimitBehaviour::Panic => {
+                assert!(x >= self.table.first().unwrap().0);
+                assert!(x <= self.table.last().unwrap().0);
+            }
+        }
 
         match self
             .table
@@ -34,12 +56,16 @@ impl LinearInterpolationTable {
 
 #[cfg(test)]
 mod tests {
+    use crate::interpolation_table::LimitBehaviour;
+
     use super::LinearInterpolationTable;
 
     #[test]
     fn test() {
-        let interpolation_table =
-            LinearInterpolationTable::new(vec![(0.0, 1.0), (2.0, 2.0), (3.0, 0.0)]);
+        let interpolation_table = LinearInterpolationTable::new(
+            LimitBehaviour::Clamp,
+            vec![(0.0, 1.0), (2.0, 2.0), (3.0, 0.0)],
+        );
         assert!((interpolation_table.get(0.0) - 1.0).abs() < 1e-10);
         assert!((interpolation_table.get(2.0) - 2.0).abs() < 1e-10);
         assert!((interpolation_table.get(3.0) - 0.0).abs() < 1e-10);
@@ -47,5 +73,8 @@ mod tests {
         assert!((interpolation_table.get(1.0) - 1.5).abs() < 1e-10);
         assert!((interpolation_table.get(2.5) - 1.0).abs() < 1e-10);
         assert!((interpolation_table.get(0.5) - 1.25).abs() < 1e-10);
+
+        assert!((interpolation_table.get(-1.0) - 1.0).abs() < 1e-10);
+        assert!((interpolation_table.get(5.0) - 0.0).abs() < 1e-10);
     }
 }
--- src/substance/water.rs
+++ src/substance/water.rs
@@ -1,8 +1,10 @@
 use uom::num_traits::Zero;
+use uom::si::available_energy::joule_per_gram;
+use uom::si::f64::AvailableEnergy;
 use uom::si::ratio::ratio;
 use uom::si::{
-    f64::{HeatCapacity, Mass, ThermodynamicTemperature, Volume},
-    heat_capacity::joule_per_kelvin,
+    f64::{SpecificHeatCapacity, Mass, ThermodynamicTemperature, Volume},
+    specific_heat_capacity::joule_per_kilogram_kelvin,
     mass::kilogram,
     temperature_interval,
     thermodynamic_temperature::kelvin,
@@ -28,8 +30,14 @@ impl Water {
         }
     }
 
-    pub fn heat_capacity() -> HeatCapacity {
-        HeatCapacity::new::<joule_per_kelvin>(4.2)
+    /// The heat capacity of water.
+    /// We treat it as the same over all temperatures and phases to avoid creating or losing energy due to moving boiling point.
+    pub fn heat_capacity() -> SpecificHeatCapacity {
+        SpecificHeatCapacity::new::<joule_per_kilogram_kelvin>(4180.0)
+    }
+
+    pub fn evaporization_energy() -> AvailableEnergy {
+        AvailableEnergy::new::<joule_per_gram>(2230.0)
     }
 
     pub fn volume(&self) -> Volume {
@@ -84,22 +92,24 @@ impl std::ops::Sub<Volume> for Water {
 }
 
 mod constants {
+    // boiling point at low pressure https://www.myengineeringtools.com/Data_Diagrams/Water_Boiling_Point_Vs_Pressure.html
+    // boiling point at high pressure https://www.engineeringtoolbox.com/water-vapor-saturation-pressure-d_599.html
+
     use lazy_static::lazy_static;
     use uom::si::{
-        f64::{MassDensity, ThermodynamicTemperature},
+        f64::{MassDensity, Pressure, ThermodynamicTemperature},
         mass_density::gram_per_cubic_centimeter,
+        pressure::millibar,
         thermodynamic_temperature::degree_celsius,
     };
 
-    use crate::interpolation_table::LinearInterpolationTable;
+    use crate::interpolation_table::{LimitBehaviour, LinearInterpolationTable};
 
     lazy_static! {
         /// Celsius -> g/cm^3
+        /// High temperatures (above 100) roughly at boiling pressure
         static ref DENSITY_BY_TEMPERATURE: LinearInterpolationTable =
-            LinearInterpolationTable::new(vec![
-                (-30.0, 0.983854),
-                (-20.0, 0.993547),
-                (-10.0, 0.998117),
+            LinearInterpolationTable::new(LimitBehaviour::Clamp, vec![
                 (0.0, 0.9998395),
                 (3.984, 0.999972),
                 (4.0, 0.999972),
@@ -124,12 +134,105 @@ mod constants {
                 (90.0, 0.96531),
                 (95.0, 0.96189),
                 (100.0, 0.95835),
+                (120.0, 0.9435),
+                (140.0, 0.9262),
+                (160.0, 0.9075),
+                (180.0, 0.887),
+                (200.0, 0.8647),
+                (225.0, 0.8338),
+                (250.0, 0.799),
+                (275.0, 0.7591),
+                (300.0, 0.7122),
+                (350.0, 0.581),
+                (373.0, 0.4434),
+                (373.946, 0.3888),
+                (400.0, 0.1222),
+                (450.0, 0.09043),
+                (500.0, 0.0765),
+                (600.0, 0.06145),
+                (700.0, 0.0526),
+                (800.0, 0.04645),
+                (900.0, 0.0418),
+                (1000.0, 0.0381),
+                (2000.0, 0.02085),
             ]);
+
+        /// mbar -> Celsius
+        /// above the critical point, we treat it as liquid
+        static ref BOILING_POINT_BY_PRESSURE_RAW: Vec<(f64, f64)> = vec![
+            (0.003, -68.0),
+            (0.017, -57.0),
+            (0.03, -51.0),
+            (0.07, -46.0),
+            (0.13, -40.0),
+            (0.17, -37.0),
+            (0.34, -31.0),
+            (0.4, -29.0),
+            (0.67, -24.0),
+            (1.33, -17.0),
+            (1.69, -14.0),
+            (3.39, -6.0),
+            (6.1, 0.0),
+            (10.16, 7.0),
+            (13.55, 12.0),
+            (16.93, 15.0),
+            (20.32, 18.0),
+            (23.71, 21.0),
+            (27.09, 22.0),
+            (30.48, 24.0),
+            (33.86, 27.0),
+            (42.33, 30.0),
+            (73.48, 40.0),
+            (123.3, 50.0),
+            (133.3, 52.0),
+            (199.1, 60.0),
+            (266.6, 67.0),
+            (311.5, 70.0),
+            (473.4, 80.0),
+            (666.6, 89.0),
+            (700.6, 90.0),
+            (846.6, 96.0),
+            (1014.2, 100.3),
+            (1433.8, 110.0),
+            (1986.7, 120.0),
+            (2702.8, 130.0),
+            (3615.4, 140.0),
+            (4761.6, 150.0),
+            (6182.3, 160.0),
+            (10028.0, 180.0),
+            (15549.0, 200.0),
+            (23196.0, 220.0),
+            (33469.0, 240.0),
+            (46923.0, 260.0),
+            (64166.0, 280.0),
+            (85879.0, 300.0),
+            (112840.0, 320.0),
+            (146010.0, 340.0),
+            (186660.0, 360.0),
+            (210440.0, 370.0),
+            (210441.0, 1e50),
+        ];
+
+        static ref BOILING_POINT_BY_PRESSURE: LinearInterpolationTable = LinearInterpolationTable::new(LimitBehaviour::Clamp, BOILING_POINT_BY_PRESSURE_RAW.clone());
+
+        static ref SATURATION_PRESSURE_BY_TEMPERATURE: LinearInterpolationTable = LinearInterpolationTable::new(LimitBehaviour::Clamp, BOILING_POINT_BY_PRESSURE_RAW.iter().copied().map(|(pressure, temperature)| (temperature, pressure)).collect());
     }
 
     pub fn density_by_temperature(temperature: ThermodynamicTemperature) -> MassDensity {
         let temperature = temperature.get::<degree_celsius>();
         let density = DENSITY_BY_TEMPERATURE.get(temperature);
         MassDensity::new::<gram_per_cubic_centimeter>(density)
+    }
+
+    pub fn boiling_point_by_pressure(pressure: Pressure) -> ThermodynamicTemperature {
+        let pressure = pressure.get::<millibar>();
+        let temperature = BOILING_POINT_BY_PRESSURE.get(pressure);
+        ThermodynamicTemperature::new::<degree_celsius>(temperature)
+    }
+
+    pub fn saturation_pressure_by_temperature(temperature: ThermodynamicTemperature) -> Pressure {
+        let temperature = temperature.get::<degree_celsius>();
+        let pressure = SATURATION_PRESSURE_BY_TEMPERATURE.get(temperature);
+        Pressure::new::<millibar>(pressure)
     }
 }
