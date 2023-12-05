45a428dc6796dfb00fd7e2bcbce6d2da031a2e22
Sebastian Schmidt <isibboi@gmail.com>
2023-12-05 16:33:33 +0200

    Add container update benchmark.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,5 @@
-use container::WaterContainer;
-use substance::water::Water;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use nuklear::{container::WaterContainer, substance::water::Water};
 use uom::si::{
     area::square_meter,
     f64::{Area, Mass, ThermodynamicTemperature, Time, Volume},
@@ -9,14 +9,9 @@ use uom::si::{
     volume::cubic_meter,
 };
 
-pub mod container;
-pub mod error;
-pub mod interpolation_table;
-pub mod substance;
-
-pub fn main() {
+fn create_container() -> WaterContainer {
     // Rectangular container of dimensions 20x2x2
-    let mut container = WaterContainer::new(
+    WaterContainer::new(
         Volume::new::<cubic_meter>(100.0),
         Area::new::<square_meter>(40.0),
         Water::new(
@@ -27,12 +22,19 @@ pub fn main() {
             Mass::new::<kilogram>(275.0),
             ThermodynamicTemperature::new::<degree_celsius>(350.0),
         ),
-    );
-
-    println!("Iteration  0: {container}");
-    for iteration in 1..=10 {
-        container.evaporate_condensate();
-        container.convect(Time::new::<second>(0.1));
-        println!("Iteration {iteration:2.}: {container}");
-    }
+    )
 }
+
+fn update_container(c: &mut Criterion) {
+    let mut container = create_container();
+
+    c.bench_function("fib 20", |b| {
+        b.iter(|| {
+            container.convect(Time::new::<second>(black_box(0.1)));
+            container.evaporate_condensate();
+        })
+    });
+}
+
+criterion_group!(benches, update_container);
+criterion_main!(benches);
